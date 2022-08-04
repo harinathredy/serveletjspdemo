20a1832130ad5255d99d5d64c8ab73b9b69b6c5a
hari <atlaharinathreddy358@gmail.com>
2022-08-04 14:44:43 +0530

    assistedprojects

---
+++ Servletconcept/src/main/java/NameServlet.java
@@ -0,0 +1,45 @@
+
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class NameServlet
+ */
+@WebServlet("/NameServlet")
+public class NameServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public NameServlet() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		String firstname=request.getParameter("firstname");
+		String lastname=request.getParameter("lastname");
+		PrintWriter out=response.getWriter();
+		out.println("Firstname= "+firstname+"Lastname= "+lastname);
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
