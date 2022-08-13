1deb4d34cdf7a2eb2a43282a7c681db2e3491542
hari <atlaharinathreddy358@gmail.com>
2022-08-13 18:26:11 +0530

    changes

---
+++ phase-2mainproject/src/main/java/model/Subject.java
@@ -0,0 +1,48 @@
+package model;
+
+public class Subject {
+	
+	private int id;
+	private String name;
+	private String shortcut;
+	
+	public Subject(int id, String name, String shortcut ) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.shortcut = shortcut;
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	
+	public String getShortcut() {
+		return shortcut;
+	}
+
+	public void setShortcut(String shortcut) {
+		this.shortcut = shortcut;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	@Override
+	public String toString() {
+		return "Subject [id=" + id + ", name=" + name + ", shortcut=" + shortcut + "]";
+	}
+	
+	
+	
+
+}
